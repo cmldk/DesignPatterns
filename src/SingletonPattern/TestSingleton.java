ade168e09a473da08221a852c6e02e815d88dc0e
cmldk <cemal.dak@gmail.com>
2020-12-01 14:03:30 +0300

    Singleton Pattern

---
+++ src/SingletonPattern/TestSingleton.java
@@ -0,0 +1,10 @@
+package SingletonPattern;
+
+public class TestSingleton {
+    public static void main(String... args){
+        Singleton s1 = Singleton.getInstance();
+
+        Singleton s2 = Singleton.getInstance();
+
+    }
+}
