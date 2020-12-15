6f6d2c657ba9ac3449b9a090d6565bf4699248c5
cmldk <cemal.dak@gmail.com>
2020-12-15 17:36:41 +0300

    Singleton Pattern

--- src/SingletonPattern/Singleton.java
+++ src/SingletonPattern/Singleton.java
@@ -1,18 +1,28 @@
 package SingletonPattern;
 
 public class Singleton {
+    // we can do it but what if dont need to use this object
+    // it may waste the memory,
+    // and this is called "eagerly creation"
+    //private static Singleton instance = new Singleton();
 
-    private static Singleton instance = null;
+    private volatile static Singleton instance;
+    //volatile: Multiple threads can access the same variable
+
 
     private Singleton(){
 
     }
 
     public static Singleton getInstance(){
+        // Double-checked locking
         if (instance == null){
-            instance = new Singleton();
+            synchronized (Singleton.class) {
+                if (instance == null){
+                    instance = new Singleton();
+                }
+            }
         }
-
         return instance;
     }
 }
--- src/SingletonPattern/TestSingleton.java
+++ src/SingletonPattern/TestSingleton.java
@@ -3,8 +3,24 @@ package SingletonPattern;
 public class TestSingleton {
     public static void main(String... args){
         Singleton s1 = Singleton.getInstance();
-
         Singleton s2 = Singleton.getInstance();
 
+        (new MyThread()).start();
+        (new Thread(new MyRunnable())).start();
+
+    }
+
+    static class MyThread extends Thread{
+        @Override
+        public void run(){
+            Singleton s1 = Singleton.getInstance();
+        }
+    }
+
+    static class MyRunnable implements Runnable {
+        @Override
+        public void run(){
+            Singleton s1 = Singleton.getInstance();
+        }
     }
 }
